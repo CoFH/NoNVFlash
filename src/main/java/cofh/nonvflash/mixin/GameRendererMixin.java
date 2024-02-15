23ac173dde9bbe80a6af04123235c92bc42d66a3
cheaterpaul <dev@paube.de>
2024-02-15 16:45:45 +0100

    check effect instance for infinite duration

--- src/main/java/cofh/nonvflash/mixin/GameRendererMixin.java
+++ src/main/java/cofh/nonvflash/mixin/GameRendererMixin.java
@@ -2,6 +2,7 @@ package cofh.nonvflash.mixin;
 
 import cofh.nonvflash.NoNVFlash;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,7 +25,7 @@ public abstract class GameRendererMixin {
         if (NoNVFlash.fadeOut.get()) {
             if (living.hasEffect(NIGHT_VISION)) {
                 int duration = living.getEffect(NIGHT_VISION).getDuration();
-                i = duration > NoNVFlash.fadeTicks.get() ? NoNVFlash.maxBrightness.get() : duration * NoNVFlash.fadeRate.get();
+                i = (duration == MobEffectInstance.INFINITE_DURATION || duration > NoNVFlash.fadeTicks.get()) ? NoNVFlash.maxBrightness.get() : duration * NoNVFlash.fadeRate.get();
             }
         }
         callback.setReturnValue((float) i);
